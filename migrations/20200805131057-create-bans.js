1745a2f1b30c76c038986d3c67bc4f169325860a
Bartosz Pazdur <b.pazdur@onet.pl>
2020-08-05 15:11:41 +0200

    Fixed bugs

--- migrations/20200805131057-create-bans.js
+++ migrations/20200805131057-create-bans.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    queryInterface.createTable('Bans', {
+    await queryInterface.createTable('Bans', {
       id: {
         allowNull: false,
         autoIncrement: true,
