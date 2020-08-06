ddc15a17a2dc1ae7490da4e08a04c4ccc5cec711
Bartosz Pazdur <b.pazdur@onet.pl>
2020-08-06 15:20:45 +0200

    Created prototype of Authorization model

---
+++ models/Authorization.js
@@ -0,0 +1,26 @@
+import Sequelize from 'sequelize'
+import connection from '../config/database'
+
+const Authorization = connection.define('authorizations', {
+    authorizationId: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+
+    authorizedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+
+    createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+
+    updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: true
+    }
+})
